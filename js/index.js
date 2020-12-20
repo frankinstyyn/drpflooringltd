b69f3d80a73400cc7e8726ea0448f1a9fdc62f4b
frankinstyyn <frankie-cormack@hotmail.com>
2020-12-20 12:31:38 +0000

    updated the maps to stop the scroll error

--- js/index.js
+++ js/index.js
@@ -1,23 +1,35 @@
-	// When the user scrolls down 80px from the top of the document, add a shadow, resize the navbar's padding and the logo's font size
-	window.onscroll = function() {scrollFunction()};
-	
-	function scrollFunction() {
-	  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-		document.getElementById("shrink").style.width = "200px";
-		$('#navBarDrop').addClass('navbarShadow');
-		$('#customNavLink').removeClass('customNav');
-		$('#customNavLink').addClass('navbarLinks a');
-	  } else {
-		document.getElementById("shrink").style.width = "230px";
-		$('#navBarDrop').removeClass('navbarShadow');
-		$('#customNavLink').addClass('customNav');
-		$('#customNavLink').removeClass('navbarLinks a');
-	  }
-	};
+// When the user scrolls down 80px from the top of the document, add a shadow, resize the navbar's padding and the logo's font size
+window.onscroll = function () {
+  scrollFunction();
+};
 
-	// IMAGE SLIDER 
-	
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("shrink").style.width = "200px";
+    $("#navBarDrop").addClass("navbarShadow");
+    $("#customNavLink").removeClass("customNav");
+    $("#customNavLink").addClass("navbarLinks a");
+  } else {
+    document.getElementById("shrink").style.width = "230px";
+    $("#navBarDrop").removeClass("navbarShadow");
+    $("#customNavLink").addClass("customNav");
+    $("#customNavLink").removeClass("navbarLinks a");
+  }
+}
 
+// IMAGE SLIDER
 
+// Disable scroll on the embedded maps
 
+var onMapMouseleaveHandler = function (event) {
+  var that = $(this);
 
+  that.on("click", onMapClickHandler);
+};
+
+var onMapClickHandler = function (event) {
+  var that = $(this);
+
+  // Disable the click handler until the user leaves the map area
+  that.off("click", onMapClickHandler);
+};
